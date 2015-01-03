e1f818fdb1b7fa873937cbdf4e33d803120a36b6
Stephen Stack <ss@stephenstack.com>
2015-01-03 17:43:08 -0600

    core: add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pylibs",
+    description="Some helpful Python code",
+    url="https://github.com/swstack/pylibs",
+    author="Stack, Stephen",
+    author_email="ss@stephenstack.com",
+    packages=find_packages(),
+    version='NA'
+)
